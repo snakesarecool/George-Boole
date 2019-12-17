033b47d09421ee557abb8f5491a07c6b549d7819
snakesarecool <57758607+snakesarecool@users.noreply.github.com>
2019-12-17 08:31:51 -0500

    game

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+var question = "Which of the following is a school color?"; 
 
+var correct = 3; //The first answer is at position 0, the last is at 3.
+
+var answers = ['Red', 'Green', 'Blue', 'Yellow']; 
+
+var answerButtons = document.getElementsByClassName("quizButton"); 
+
+for(var spot=0; spot<answers.length; spot++)
+{
+    answerButtons[spot].innerHTML = answers[spot]; 
+
+    answerButtons[spot].addEventListener('click', function() {
+        disableAll(); 
+        if(this.innerHTML == answers[correct]) {
+            this.setAttribute('style','background-color:rgb(76, 175, 80);')
+        }else {
+            this.setAttribute('style','background-color:rgb(210, 50, 35);')
+           
+        }
+    }); 
+
+}
+
+document.getElementById("question").innerHTML = question; 
+
+function changeColor() {
+document.getElementById('demo2').setAttribute('style','background-color:rgb(210,50,35);')
+}
+
+function disableAll() {
+    for(var num=0; num<answerButtons.length; num++) {
+        answerButtons[num].setAttribute('disabled', true); 
+    }
+}
